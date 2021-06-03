5ede6645ef99393bcf2ec00ea1727d72f907a220
Donghyeon Kim <0916dhkim@gmail.com>
2021-06-02 23:41:51 -0400

    Add Instagram Login WebView

--- components/InstagramLogin.tsx
+++ components/InstagramLogin.tsx
@@ -1,19 +1,17 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
+import WebView from 'react-native-webview';
 
 export default function InstagramLogin() {
+  const base = 'https://api.instagram.com/oauth/authorize';
+  const searchParams = new URLSearchParams({
+    client_id: process.env.INSTAGRAM_CLIENT_ID ?? '',
+    redirect_uri: 'https://localhost/',
+    scope: 'user_profile,user_media',
+    response_type: 'code',
+  });
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+      <WebView source={{ uri: `${base}?${searchParams.toString()}` }} />
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
